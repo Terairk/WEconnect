f426337644687f65e7acd8f3bb8e96c3009d9407
Terairk <terairkgaming@gmail.com>
2020-09-17 20:40:45 +0800

    Add some basic ui and an imbed google forms

--- app.py
+++ app.py
@@ -21,9 +21,30 @@ posts = [
 def home():
     return render_template("home.html", posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/employers')
+def employer():
+    return render_template("employer.html")
+
+
+@app.route('/students')
+def student():
+    return render_template("students.html")
 
 if __name__ == '__main__':
     app.run()
